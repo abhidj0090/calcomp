9496a99aba3bf131d302ae727e4cfed191393aa8
abhidj0090 <37368261+abhidj0090@users.noreply.github.com>
2021-06-23 11:39:18 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,10 +153,14 @@ jQuery(function ($) {
 	}
 });
 function slugify(Text) {
-    return Text
-	.split("/")
-	.filter(function (c) { return c.length;})
-	.pop();
+	return text
+    .toString()                     // Cast to string
+    .toLowerCase()                  // Convert the string to lowercase letters
+    .normalize('NFD')      			// The normalize() method returns the Unicode Normalization Form of a given string.
+    .trim()                         // Remove whitespace from both sides of a string
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-');        // Replace multiple - with single -
   }
 /* ========================================================================= */
 /*	Download event
