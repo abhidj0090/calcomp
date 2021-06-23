8898dd8eafe1c85c3042cce056ec1a2796b45041
abhidj0090 <37368261+abhidj0090@users.noreply.github.com>
2021-06-23 17:10:33 +0530

    Update fetch_likes.js

--- my_functions/fetch_likes.js
+++ my_functions/fetch_likes.js
@@ -1,7 +1,7 @@
 // Credit to Josh Comeau for coming up with the idea - Edited by Abhinaba
 const faunadb = require('faunadb');
 exports.handler = async (event, context) => {
-  const context = process.env.CONTEXT
+  const envcontext = process.env.CONTEXT
   const q = faunadb.query;
   const client = new faunadb.Client({
     secret: process.env.FAUNAdj_SECRET_KEY,
@@ -39,7 +39,7 @@ exports.handler = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify({
       likes: document.data.likes,
-      process:context,
+      process:envcontext,
     }),
   };
 };
